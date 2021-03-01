4a9876b736f6eeb121c26bbc4ce9b410d934bff3
ZukanTheHan <70758694+ZukanTheHan@users.noreply.github.com>
2021-03-01 16:14:12 +0300

    Create SQLQuery1.sql

---
+++ ReCapProjectDay9/SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿Create Table Brands(
+		Id int primary key identity(1,1),
+		BrandName nvarchar (50),
+
+)
+
+Create Table Colors(
+		ColorId int primary key identity(1,1),
+		ColorName nvarchar (50),
+
+)
+
+Create Table Cars(
+		Id int primary key identity (1,1),
+		BrandId int,
+		ColorId int,
+		ModelYear int,
+		DailyPrice decimal,
+		Descriptions nvarchar (50)
+		Foreign key (BrandId) references Brand(BrandId),
+		Foreign key (ColorId) references Color(ColorId)
+)
